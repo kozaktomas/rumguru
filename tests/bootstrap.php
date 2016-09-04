80d082e8a5f7b1816af8f90582beb4249ffb7b6f
Tomas Kozak <kozak@talko.cz>
2016-09-04 10:44:06 +0200

    Fixed bootstrap fo tests.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,17 +2,4 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$configurator = new Nette\Configurator;
 
-$configurator->setTimeZone('Europe/Prague');
-$configurator->setTempDirectory(__DIR__ . '/../temp');
-
-$configurator->createRobotLoader()
-	->addDirectory(__DIR__ . "/../app")
-	->register();
-
-$configurator->addConfig(__DIR__ . '/../app/config/config.neon');
-
-$container = $configurator->createContainer();
-
-return $container;
